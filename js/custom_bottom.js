45a4616a24ca7045b06c85a42a047f1d47b45446
Ascotbe <869335729@qq.com>
2022-12-07 16:26:59 +0800

    Site updated: 2022-12-07 16:26:58

--- js/custom_bottom.js
+++ js/custom_bottom.js
@@ -1,7 +1,3 @@
-// 动态心跳
-$(document).ready(function(e){
-    $('.copyright').html('©2021 - 2022 <i class="fa-fw fas fa-heartbeat fa-beat" style="color: #f92672;"></i> By 花猪');
-})
 
 $(document).ready(function(e){
     show_date_time();
